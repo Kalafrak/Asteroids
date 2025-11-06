834a7797a4687617c2afec1d1806f21227b79143
Kalafrak <mdelldeadstar@hotmail.co.uk>
2025-11-06 17:17:02 +0000

    add logger file

--- main.py
+++ main.py
@@ -45,4 +45,7 @@ def main():
         pygame.display.flip()
 
 if __name__ == "__main__":
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     main()
